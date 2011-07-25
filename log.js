0f320db4f427d925b025c6fb023a8f0e0f4957d1
Simon Pratt <spratt@scs.carleton.ca>
2011-07-25 14:43:10 -0400

    Now allowing autoscroll to be disabled

--- log.js
+++ log.js
@@ -1,13 +1,21 @@
 var log_div = document.getElementById("log_div");
 var response;
 var log_xh=new XMLHttpRequest();
+var autoscroll=true;
+var enable_autoscroll = function() {
+    autoscroll=true;
+};
+var disable_autoscroll = function() {
+    autoscroll=false;
+};
 var logUpdate = function() {
     console.log("log_xh: Ready state change...");
     if (log_xh.readyState==4 && log_xh.status==200) {
 	console.log("log_xh: Status: 200");
 	log_div.innerText=log_xh.responseText;
 	log_div.style.visibility="visible";
-	log_div.scrollTop=log_div.scrollHeight;
+	if(autoscroll)
+	    log_div.scrollTop=log_div.scrollHeight;
     }
 }
 var refresh_log = function() {

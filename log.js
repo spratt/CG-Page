5f5bc2fca330af3721b2f9febcdf98f35a7f70f7
Simon Pratt <spratt@scs.carleton.ca>
2011-07-25 14:47:15 -0400

    Now only showing appropriate enable/disable button

--- log.js
+++ log.js
@@ -2,11 +2,17 @@ var log_div = document.getElementById("log_div");
 var response;
 var log_xh=new XMLHttpRequest();
 var autoscroll=true;
+var enable_button = document.getElementById("enable_button");
+var disable_button = document.getElementById("disable_button");
 var enable_autoscroll = function() {
     autoscroll=true;
+    disable_button.style.visibility="visible";
+    enable_button.style.visibility="hidden";
 };
 var disable_autoscroll = function() {
     autoscroll=false;
+    disable_button.style.visibility="hidden";
+    enable_button.style.visibility="visible";
 };
 var logUpdate = function() {
     console.log("log_xh: Ready state change...");
@@ -24,5 +30,6 @@ var refresh_log = function() {
     log_xh.open("GET","log/pnd.log",true);
     log_xh.send();
 }
+enable_autoscroll();
 refresh_log();
 setInterval(refresh_log,5000);
